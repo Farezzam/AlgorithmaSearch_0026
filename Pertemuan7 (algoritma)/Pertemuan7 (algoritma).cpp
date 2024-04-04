251ed3ffab66cc2e84c5e3e386c9b830199a9372
ASUS <faris13abiyu@gmail.com>
2024-04-04 19:21:27 +0700

    for in void input

--- Pertemuan7 (algoritma)/Pertemuan7 (algoritma).cpp
+++ Pertemuan7 (algoritma)/Pertemuan7 (algoritma).cpp
@@ -20,5 +20,10 @@ void input()
 	cout << "\n-------------------\n";
 	cout << " Enter array element \n";
 	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
 
